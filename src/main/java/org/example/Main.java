3f6b71810fa84f74aeda2615c30fe2db2f0f7208
Dayana Ortiz <karendortiz@ucundinamarca.edu.co>
2025-03-05 19:16:49 -0500

    Agregue la otra parte del codigo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -93,3 +93,52 @@ class Biblioteca {
         }
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Biblioteca biblioteca = new Biblioteca();
+        int opcion;
+
+        do {
+            System.out.println("Biblioteca D&E");
+            System.out.println("1. Ingresar un nuevo libro");
+            System.out.println("2. Buscar libro por el título");
+            System.out.println("3. Buscar libro por el autor");
+            System.out.println("4. Lista de todos los libros");
+            System.out.println("5. Salir");
+            System.out.print("Elije una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese el título: ");
+                    String titulo = scanner.nextLine();
+                    System.out.print("Ingrese el autor: ");
+                    String autor = scanner.nextLine();
+                    System.out.print("Ingrese el codigo: ");
+                    String codigo = scanner.nextLine();
+                    biblioteca.agregarLibro(new Libro(titulo, autor, codigo));
+                    break;
+                case 2:
+                    System.out.print("Ingrese el título que desea buscar: ");
+                    String tituloBuscar = scanner.nextLine();
+                    biblioteca.buscarLibroPorTitulo(tituloBuscar);
+                    break;
+                case 3:
+                    System.out.print("Ingrese el autor que desea buscar: ");
+                    String autorBuscar = scanner.nextLine();
+                    biblioteca.buscarLibroPorAutor(autorBuscar);
+                    break;
+                case 4:
+                    biblioteca.listarLibros();
+                    break;
+                case 5:
+                    System.out.println("Estas saliendo de la biblioteca ");
+                    break;
+                default:
+                    System.out.println("Esta opción no es válida. Intentelo de nuevo.");
+            }
+        } while (opcion != 5);
+    }
+}
